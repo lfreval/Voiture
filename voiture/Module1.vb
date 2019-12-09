2a146c56acde192eafb525fd5a282fbdd20ede53
Laurent Fréval <laurent.freval@outlook.com>
2019-12-09 11:28:22 +0100

    exemple 1 : diapo 54

--- voiture/Module1.vb
+++ voiture/Module1.vb
@@ -1,7 +1,17 @@
 ﻿Module Module1
 
-    Sub Main()
+    Dim voiTure As New Hashtable
 
+    Sub parasite()
+        voiTure("kilometrage") = -30
+    End Sub
+
+    Sub Main()
+        voiTure.Add("kilometrage", 0)
+        voiTure("kilometrage") += 12
+        parasite()
+        Console.WriteLine(voiTure("kilometrage"))
+        Console.ReadKey()
     End Sub
 
 End Module
