1e6a750e4267511a0bd330e529416a95028341f4
Laurent Fréval <laurent.freval@outlook.com>
2019-12-09 13:40:29 +0100

    exemple 2 : POO sans encapsulation

--- voiture/VoitureSale.vb
+++ voiture/VoitureSale.vb
@@ -1,3 +1,17 @@
 ﻿Public Class VoitureSale
-    Public km As Integer = 0
+    Private _kilometrage As Integer = 0
+    Public Property km As Integer
+        Get
+            Return _kilometrage
+        End Get
+        Set(value As Integer)
+            _kilometrage = value
+        End Set
+    End Property
+
+    Public Function Rouler(km As Integer) As Integer
+        _kilometrage += km
+        Return _kilometrage
+    End Function
+
 End Class
